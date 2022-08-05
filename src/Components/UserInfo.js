8306754657415055b36ff5acdbfa8eb63bd432c7
Juan Camilo <jccifuentes21@gmail.com>
2022-08-05 09:29:14 -0700

    working on the user form for the twoot

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./UserInfo.module.css";
 
+const UserInfo = (props) => {
+  const [user, setUser] = useState("John Doe");
+  const [username, setUsername] = useState("john-doe");
+  const [isEditing, setIsEditing] = useState(false);
 
-const UserInfo = (props) =>{
   return (
     <div className={classes.userInfo}>
       <img
         className={classes.userIcon}
-        src="https://avatars.dicebear.com/api/bottts/something.svg"
+        src={`https://avatars.dicebear.com/api/bottts/${username}.svg`}
         alt="User icon of a robot generated automatically"
       ></img>
-      <div className={classes.user}>
-        <strong>John</strong> Doe
+      <div>
+        <span className={classes.user}>{user}</span>
+        <span
+          className={`${classes.editButton} fa-solid fa-pen-to-square`}
+        ></span>
       </div>
-      <div className={classes.username}>@JohnDoe</div>
+      <span className={classes.username}>@{username}</span>
     </div>
   );
-}
+};
 
-export default UserInfo
+export default UserInfo;
